bd7ff9abe640c8672cb369c9e9d1afa23b71c9bf
Leonardo Graboski Veiga <leogveiga@gmail.com>
2016-04-22 17:29:29 -0300

    some crazy error

--- comandosLatex.sh
+++ comandosLatex.sh
@@ -5,4 +5,5 @@ bibtex TCCmain
 pdflatex --interaction=nonstopmode --draftmode TCCmain.tex
 #pdflatex --interaction=nonstopmode --draftmode TCCmain.tex
 pdflatex --interaction=nonstopmode -synctex=1 TCCmain.tex
+rm TCCmain.pdf
 evince TCCmain.pdf &
