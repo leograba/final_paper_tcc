f18c5f4b2869927f01bb8aedd4c67bfd4dd0f0d7
Leonardo Graboski Veiga <leogveiga@gmail.com>
2016-04-22 20:38:20 -0300

    Description of the mechanical system added

--- comandosLatex.sh
+++ comandosLatex.sh
@@ -1,9 +1,9 @@
 #bin/bash
 
+rm TCCmain.pdf
 pdflatex --interaction=nonstopmode --draftmode TCCmain.tex
 bibtex TCCmain 
 pdflatex --interaction=nonstopmode --draftmode TCCmain.tex
 #pdflatex --interaction=nonstopmode --draftmode TCCmain.tex
 pdflatex --interaction=nonstopmode -synctex=1 TCCmain.tex
-rm TCCmain.pdf
 evince TCCmain.pdf &
